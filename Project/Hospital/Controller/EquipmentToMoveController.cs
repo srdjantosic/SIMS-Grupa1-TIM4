a6b31856953623cbc1ebb0558dfe8fa17cf86c04
Srdjan Tosic <stosic2000@gmail.com>
2022-05-03 22:48:58 +0200

    MoveEquipment funkcionalnost dovrsena

---
+++ Project/Hospital/Controller/EquipmentToMoveController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Hospital.Service;
+using Project.Hospital.Model;
+namespace Project.Hospital.Controller
+{
+    public class EquipmentToMoveController
+    {
+        private EquipmentToMoveService equipmentToMoveService;
+
+
+        public EquipmentToMoveController(EquipmentToMoveService equipmentToMoveService)
+        {
+            this.equipmentToMoveService = equipmentToMoveService;
+        }
+        public void changeEquipment(String Id, String oldRoomId, String Name, int Quantity, DateTime dateTime, String newRoomID) {
+           equipmentToMoveService.changeEquipment(Id, oldRoomId, Name, Quantity, dateTime, newRoomID); 
+        }
+        public List<EquipmentToMove> ShowEquipment()
+        {
+            return equipmentToMoveService.ShowEquipment();
+        }
+    }
+}
